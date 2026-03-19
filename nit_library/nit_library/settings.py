bcc89052bcc44171f0beaa83031d074a5a067c32
Rishabh Singh <rishabh.singh.2005.8@gmail.com>
2026-03-19 17:43:59 +0530

    settings updated

--- nit_library/nit_library/settings.py
+++ nit_library/nit_library/settings.py
@@ -85,3 +85,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# --- Ye block end mein jayega ---
+CSRF_TRUSTED_ORIGINS = [
+    'https://narula-library.onrender.com',
+    'https://*.onrender.com'
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+# -------------------------------
